a3b0a59534c75ebc0f6a690a72ba83050f7d4b53
DimiPetrov <d_petrov@yahoo.com>
2020-09-24 15:46:54 +0300

    three files

--- Conditional Statements/three.js
+++ Conditional Statements/three.js
@@ -5,29 +5,28 @@ function solve(arg1, arg2, arg3) {
 
     let max = Math.max(num1, num2, num3);
 
-    console.log(max);
-    if(max === num1){
-        if(num2 > num3){
-            console.log("blah blah");
-        }else{
-
+    if (max === num1) {
+        if (num2 > num3) {
+            console.log(num1 + " " + num2 + " " + num3);
+        } else {
+            console.log(num1 + " " + num3 + " " + num2);
         }
-    }else if (max === num2){
-        if(num1 > num3){
-
-        }else{
-            
-        }
-
-    }else if (max === num3){
-        if(num1 > num2){
-            console.log(num3 + " " + num1 + " " +num2);
-        }else{
-            console.log(num3 + " " + num2 + " " +num1);
+    } else if (max === num2) {
+        if (num1 > num3) {
+            console.log(num2 + " " + num1 + " " + num3);
+        } else {
+            console.log(num3 + " " + num2 + " " + num1);
         }
 
-    } 
-    
+    } else if (max === num3) {
+        if (num1 > num2) {
+            console.log(num3 + " " + num1 + " " + num2);
+        } else {
+            console.log(num3 + " " + num2 + " " + num1);
+        }
+
+    }
+
 }
 
-solve(-0.5, -10, 0)
+solve(3, 2, 1)
