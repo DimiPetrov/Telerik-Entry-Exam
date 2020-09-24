01f9a5ae56229e03f8061afdb03c329f673f483b
Samuel Eustachi <eustachi@gmail.com>
2020-09-24 11:30:01 +0200

    fixing three

--- Conditional Statements/three.js
+++ Conditional Statements/three.js
@@ -3,13 +3,31 @@ function solve(arg1, arg2, arg3) {
     let num2 = Number(arg2);
     let num3 = Number(arg3);
 
-    if(num1 >= num2 && num1 >= num3) {
-        console.log(num1);
-    } else if(num2 >= num1 && num2 >= num3) {
-        console.log(num2);
-    } else if(num3 >= num1 && num3 >= num2) {
-        console.log(num3)
-    }
+    let max = Math.max(num1, num2, num3);
+
+    console.log(max);
+    if(max === num1){
+        if(num2 > num3){
+            console.log("blah blah");
+        }else{
+
+        }
+    }else if (max === num2){
+        if(num1 > num3){
+
+        }else{
+            
+        }
+
+    }else if (max === num3){
+        if(num1 > num2){
+            console.log(num3 + " " + num1 + " " +num2);
+        }else{
+            console.log(num3 + " " + num2 + " " +num1);
+        }
+
+    } 
+    
 }
 
 solve(-0.5, -10, 0)
